26a6f94849239c612fd4c619eec7bd8eebf09aa1
lab04 <lab04@lab4-18>
2024-02-26 22:10:18 -0300

    1.1

--- ProjetoCalc/src/ClassePrincipal.java
+++ ProjetoCalc/src/ClassePrincipal.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		
+		// AULA PARA APENAS MOSTRAR FUNÇOES DO GITHUB E GIT
 		while (true){
 			System.out.println("Informe a calculadora:");
 			Scanner sc = new Scanner(System.in);
 			int numero = sc.nextInt();
+			
 			
 			for(int i=0; i<=10; i++){
 				
 				System.out.printf(numero + " * " + i + " = ");
 				System.out.println(numero * i);
 			}
+			
 			
 			for(int i=0; i<=10; i++){
 				
